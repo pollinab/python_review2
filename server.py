f9f7ee7ac25e5f353413a91862feaa01901dc62a
pollinab <44142702+pollinab@users.noreply.github.com>
2019-05-02 20:02:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,46 +1,105 @@
 import flask
 import argparse
 from flask import request
+import sqlite3
 
 app = flask.Flask('client')
 
-
-@app.route('/', methods=['POST', 'GET'])
-def show_page():
-    return 'OK'
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE if not exists trips
+                  (trip_id text, city text, departure text,
+                   arrival text)
+               """)
 
 
 @app.route('/add_trip', methods=['POST'])
 def new_trip():
-    return 'OK'
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT trip_id FROM trips WHERE trip_id = ?",
+                   [request.form['id']])
+    if cursor.fetchone() is None:
+        trip = (request.form['id'], request.form['city'],
+                request.form['departure'], request.form['arrival'])
+        cursor.execute("INSERT INTO trips VALUES (?, ?, ?, ?)", trip)
+        conn.commit()
+        conn.close()
+        return 'OK'
+    conn.close()
+    return 'ERROR: this id already exists'
 
 
 @app.route('/update_trip', methods=['POST'])
 def update_trip():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT trip_id FROM trips WHERE trip_id = ?",
+                   [request.form['id']])
+    if cursor.fetchone() is None:
+        return 'ERROR: id not found'
+    if request.form.get('city') is not None:
+        cursor.execute("UPDATE trips SET city = ? WHERE trip_id = ?",
+                       (request.form['city'], request.form['id']))
+    if request.form.get('departure') is not None:
+        cursor.execute("UPDATE trips SET departure = ? WHERE trip_id = ?",
+                       (request.form['departure'], request.form['id']))
+    if request.form.get('arrival') is not None:
+        cursor.execute("UPDATE trips SET arrival = ? WHERE trip_id = ?",
+                       (request.form['arrival'], request.form['id']))
+    conn.commit()
+    conn.close()
     return 'OK'
 
 
 @app.route('/delete_trip', methods=['POST'])
 def delete_trip():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM trips WHERE trip_id = ?",
+                   [request.form['id']])
+    conn.commit()
+    conn.close()
     return 'OK'
 
 
 @app.route('/show_trip', methods=['GET'])
 def show_trip():
-    print(request.form['id'])
-    return 'OK'
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM trips WHERE trip_id = ?",
+                   [request.form['id']])
+    trip = cursor.fetchone()
+    conn.close()
+    if trip is None:
+        return 'ERROR: id not found'
+    keys = ('trip_id', 'city', 'departure', 'arrival')
+    return flask.jsonify(dict(zip(keys, trip)))
 
 
 @app.route('/find_trip', methods=['GET'])
 def find_trip():
-    return 'OK'
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    c = request.form.get('city')
+    d = request.form.get('departure')
+    a = request.form.get('arrival')
+    cursor.execute("""SELECT * FROM trips 
+                    WHERE (? IS NULL OR city = ?)
+                    AND (? IS NULL OR departure = ?)
+                    AND (? IS NULL OR arrival = ?)""",
+                   (c, c, d, d, a, a))
+    trips = cursor.fetchall()
+    conn.close()
+    if not trips:
+        return 'Not found'
+    length = len(trips)
+    return flask.jsonify({i: trips[i] for i in range(length)})
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--host', default='localhost')
 parser.add_argument('--port', default=50001, type=int)
 args = parser.parse_args()
-
-app.run('::', args.port, debug=True, threaded=True)
-
-
+app.run(args.host, args.port, debug=True, threaded=True)
 
